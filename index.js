12864f9e0962ea2145ff76c2e8c6b50d97861d99
Coral <coralpeony21@gmail.com>
2023-02-18 20:29:31 +0200

    added function that generates a license badge based on the selection

--- index.js
+++ index.js
@@ -60,7 +60,24 @@ const questions = [
     },
   ];
 
-
+  // function for badges based on license selection.
+function badge(value) {
+    if (value === "Apache License 2.0") {
+      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    } else if (value === "GNU General Public License v3.0") {
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    } else if (value === "MIT License") {
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    } else if (value === "Boost Software License 1.0") {
+      return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+    } else if (value === "Eclipse Public License 2.0") {
+      return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
+    }  else if (value === "Mozilla Public License 2.0") {
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    } else {
+        return "No license selected.";
+    }
+  }
 
 
 
